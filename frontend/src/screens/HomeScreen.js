22a033563825686ddc4a58a29711525c3b565a15
Duke Capitan <dukecapitan@gmail.com>
2020-12-29 00:44:13 +0100

    search box and searching

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { listProducts } from '../actions/productAction';
@@ -8,25 +8,44 @@ function HomeScreen (props) {
     const productList = useSelector(state => state.productList);
     const { products, loading, error } = useSelector(state => state.productList);
 
+    const [ search, setSearch ] = useState();
+    const [ searchFlag, setSearchFlag ] = useState(false);
+
     const dispatch = useDispatch();
+
+    const searchHandler = (e) => {
+
+        setSearch(e.target.value.toLowerCase());
+
+        if(e.target.value.length > 0){
+            setSearchFlag(true);
+        } else {
+            setSearchFlag(false);
+        }
+
+        
+    }
+    
     
     useEffect(() => {
         
         dispatch(listProducts());
         
         return () => {
+
             
         }
     }, [])
 
+    
 
-    return  loading ? <div>Loading......</div> :
-    error ? <div>{error}</div> : <ul className="products">
-        
-        {
-            
-        products.map(product => 
-            <li key={product._id}> 
+    let searching = [];
+    let notFound = "";
+   
+    
+    products.map(product => {
+        if( search === product.name.toLowerCase() ){
+            searching.push(<li key={product._id}> 
                 <div className="product">
                     <Link to={'/details/' + product._id}>
                         <img className="product-image" src={product.image} alt="product" />
@@ -40,9 +59,47 @@ function HomeScreen (props) {
                     <div className="product-price">€{product.price}</div>
                     <div className="product-rating">{product.rating} Stars {product.reviews}</div>
                 </div>
-            </li>)
+            </li>);
+        } else {
+            notFound = <div>Not Found</div>
         }
-    </ul> 
+    })
+
+    
+
+
+    return  loading ? <div>Loading......</div> :
+    error ? <div>{error}</div> : <div className="home-with-search">
+
+        <div className="search-bar">
+            <input type="text" name="search" onChange={searchHandler} />
+        </div>
+
+        <ul className="products">
+
+            {searchFlag ? searching : products.map(product => 
+                <li key={product._id}> 
+                    <div className="product">
+                        <Link to={'/details/' + product._id}>
+                            <img className="product-image" src={product.image} alt="product" />
+                        </Link>
+                        <div className="product-name">
+                            <Link to={'/details/' + product._id}>
+                                {product.name}
+                            </Link>
+                        </div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">€{product.price}</div>
+                        <div className="product-rating">{product.rating} Stars {product.reviews}</div>
+                    </div>
+                </li>)}
+            
+        </ul>
+
+
+    </div>
+    
+     
 }
 
 export default HomeScreen;
