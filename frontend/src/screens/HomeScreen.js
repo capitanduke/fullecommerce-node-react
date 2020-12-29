1bad99d4436afa6030523ecd9c16e9f603761e2f
Duke Capitan <dukecapitan@gmail.com>
2020-12-29 22:09:13 +0100

    serach by categories

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,9 @@ function HomeScreen (props) {
     const { products, loading, error } = useSelector(state => state.productList);
 
     const [ search, setSearch ] = useState();
+    const [ categories, setCategories ] = useState();
     const [ searchFlag, setSearchFlag ] = useState(false);
+    const [ categoriesFlag, setCategoriesFlag ] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -22,7 +24,6 @@ function HomeScreen (props) {
         } else {
             setSearchFlag(false);
         }
-
         
     }
     
@@ -40,7 +41,6 @@ function HomeScreen (props) {
     
 
     let searching = [];
-    let notFound = "";
    
     
     products.map(product => {
@@ -60,12 +60,40 @@ function HomeScreen (props) {
                     <div className="product-rating">{product.rating} Stars {product.reviews}</div>
                 </div>
             </li>);
-        } else {
-            notFound = <div>Not Found</div>
-        }
+        } 
     })
 
-    
+    const categoriesHandler = (e) => {
+        setCategories(e.target.value);
+
+        setCategoriesFlag(true);
+        
+        if( e.target.value === "all"){
+            setCategoriesFlag(false);
+        }
+    }
+
+    let searchCategories = [];
+
+    products.map(product => {
+        if( categories === product.category ){
+            searchCategories.push(<li key={product._id}> 
+                <div className="product">
+                    <Link to={'/details/' + product._id}>
+                        <img className="product-image" src={product.image} alt="product" />
+                    </Link>
+                    <div className="product-name">
+                        <Link to={'/details/' + product._id}>
+                            {product.name}
+                        </Link>
+                    </div>
+                    <div className="product-brand">{product.brand}</div>
+                    <div className="product-price">€{product.price}</div>
+                    <div className="product-rating">{product.rating} Stars {product.reviews}</div>
+                </div>
+            </li>);
+        } 
+    })
 
 
     return  loading ? <div>Loading......</div> :
@@ -73,11 +101,23 @@ function HomeScreen (props) {
 
         <div className="search-bar">
             <input type="text" name="search" onChange={searchHandler} />
+
+            <select name="categories" onChange={categoriesHandler}>
+                <option value="all">Select category</option>
+                {products.map(product => 
+                    <option key={product._id} value={product.category}>{product.category}</option>
+                )}
+                
+            </select>
+
+            
+
+
         </div>
 
         <ul className="products">
 
-            {searchFlag ? searching : products.map(product => 
+            {searchFlag ? searching : categoriesFlag ? searchCategories : products.map(product => 
                 <li key={product._id}> 
                     <div className="product">
                         <Link to={'/details/' + product._id}>
@@ -92,7 +132,9 @@ function HomeScreen (props) {
                         <div className="product-price">€{product.price}</div>
                         <div className="product-rating">{product.rating} Stars {product.reviews}</div>
                     </div>
-                </li>)}
+            </li>)}
+
+           
             
         </ul>
 
