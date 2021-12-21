7a2d93e8febc9b724a2c6cc83f535a1be37c080a
Raul <dukecapitan@gmail.com>
2021-12-21 21:42:40 +0100

    server exact as video

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,41 @@
-import dotenv from 'dotenv';
-import config from './config';
+import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import userRoute from './routes/userRoute';
-import productRoute from './routes/productRoute';
-import orderRoute from './routes/orderRoute';
-import uploadRouter from './routes/uploadRouter';
+import dotenv from 'dotenv';
 import path from 'path';
-
-var express = require('express');
-
+import productRouter from './routes/productRoute.js';
+import userRouter from './routes/userRoute.js';
+import orderRouter from './routes/orderRoute.js';
+import uploadRouter from './routes/uploadRouter.js';
 dotenv.config();
-
-const mongodbUrl = config.MONGODB_URL;
-
-mongoose.connect(mongodbUrl, {
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}).catch(error => console.log(error.reason));
-
 const app = express();
-
-app.use(bodyParser.json());
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 app.use('/api/uploads', uploadRouter);
-
-app.use("/api/users", userRoute);
-
-app.use("/api/products", productRoute);
-
-app.use('/api/orders', orderRoute);
-
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 const __dirname = path.resolve();
-
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
-
-/*app.get('/', (req, res) => {
-    res.send('Serve is ready');
-});*/
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
 app.use((err, req, res, next) => {
-    res.status(500).send({message: err,message});
+  res.status(500).send({ message: err.message });
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
