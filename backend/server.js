dde680f07cbaca695ceeffceb9630821cf959412
Raul <dukecapitan@gmail.com>
2022-01-10 22:56:50 +0100

    server database name

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/myFirstDatabase', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
